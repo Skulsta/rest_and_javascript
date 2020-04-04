69e47104134c0a58edaaa9129290b50ed671d46c
skulsta <sti.sku@gmail.com>
2020-04-04 14:42:32 +0200

    Added morgan for logging

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+app.use(morgan('dev'));
 app.get('/', (request, response) => response.send('Hello World'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-

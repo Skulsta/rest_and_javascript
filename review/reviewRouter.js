fa2d7585c81b176e0b5061a5a474f1cea2da7d78
skulsta <sti.sku@gmail.com>
2020-04-06 16:31:30 +0200

    reviewRouter can now post new reviews to the server.

--- review/reviewRouter.js
+++ review/reviewRouter.js
@@ -6,4 +6,12 @@ reviewRouter.get('/', (req, res) =>  {
   res.json(reviewService.getAll());
 });
 
+reviewRouter.post('/', (req, res) => {
+  const content =  req.body.content;
+  const score =  req.body.score;
+  const relatedItemId =  req.body.relatedItemId;
+  const review = reviewService.create(content, score, relatedItemId);
+  res.status(201).json(review);
+})
+
 module.exports = reviewRouter;

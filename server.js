1ad6730d6c3f93d7be3d590da0464600f94d4f62
skulsta <sti.sku@gmail.com>
2020-04-06 09:22:28 +0200

    Added class for movie reviews

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const tvShowRouter = require('./tvShow/tvShowRouter');
+const reviewRouter = require('./review/reviewRouter');
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ const port = 3000;
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/tvshow', tvShowRouter);
+app.use('/review', reviewRouter);
 
 app.get('/', (request, response) => response.send('Hello World'));
 
